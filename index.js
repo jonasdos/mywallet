8d97ef965f35cf8c9dbcdbe79e6add580425df1c
Jonas Dos <jonas.dsds@hotmail.com>
2024-10-07 14:16:15 -0300

    feat. Porta.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(authRouter)
 app.use(transactionRouther)
-const porta = process.env.PORT
+const porta = process.env.PORT || 5000
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta ${porta}`)
 })
